e0aed5cab7828330898613d1540824945d221b5c
Zoe Woo <zoewoo@MacBook-Pro-8.local>
2020-09-30 00:35:26 -0500

    Change members and connect mysql

--- src/main/java/com/flexon/app/User.java
+++ src/main/java/com/flexon/app/User.java
@@ -5,67 +5,91 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class User {
-	private String Email;
-	private String Password;
-	private String Name;
-	private String Birthday;
-	private String Gender;
-	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int phoneNum;
+	private String emailAdd;
+	private String password;
 	
 	public User() {
 		
 	}
-	public User(String e, String p, String n, String b, String g) {
-		this.Email = e;
-		this.Password = p;
-		this.Name = n;
-		this.Birthday = b;
-		this.Gender = g;
-	}
-	public String getEmail() {
-		return Email;
-	}
-	public void setEmail(String email) throws MyException {
-		if(!email.matches("^(.+)@(.+)$")) {
-			throw new MyException("Input email not valid!");
-		}
-		this.Email = email;
-	}
-	public String getPassword() {
-		return Password;
-	}
-	public void setPassword (String password) throws MyException {
-		if(!password.matches("((?=.*[a-z])(?=.*d)(?=.*[@#$%])(?=.*[A-Z]).{6,16})")){
-			throw new MyException("Input Password not valid!");
-		}
-		this.Password = password;
-	}
-	public String getName() {
-		return Name;
-	}
-	public void setName(String name) throws MyException {
-		if(!name.matches("^[_a-zA-Z0-9]{6,18}$")) {
-			throw new MyException("Input UserName not valid!");
-		}
-		Name = name;
-	}
-	public String getBirthday() {
-		return Birthday;
-	}
-	public void setBirthday(String birthday) throws MyException{
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		String today = dateFormat.format(new Date());
-		if(today.compareTo(birthday) <= 0) {
-			throw new MyException("Input date not valid");
-		}
-		this.Birthday = birthday;
-	}
-	public String getGender() {
-		return Gender;
-	}
-	public void setGender(String gender) {
-		Gender = gender;
+	public User(String e, String p, String fn, String ln, int pn) {
+		this.id = -1;
+		this.emailAdd = e;
+		this.password = p;
+		this.firstName = fn;
+		this.lastName = ln;
+		this.phoneNum = pn;
 	}
 	
+	public int getId() {
+		return this.id;
+	}
+	
+	public boolean setId(int num) {
+		if(num <= 0) {
+			return false;
+		}
+		this.id = num;
+		return true;
+	}
+	
+	public String getEmail() {
+		return emailAdd;
+	}
+	
+	public boolean setEmail(String email){
+		if(!email.matches("^(.+)@(.+)$")) {
+			return false;
+		}
+		this.emailAdd = email;
+		return true;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public boolean setPassword (String password){
+		if(!password.matches("((?=.*[a-z])(?=.*d)(?=.*[@#$%])(?=.*[A-Z]).{6,16})")){
+			return false;
+		}
+		this.password = password;
+		return true;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	public boolean setFirstName(String name){
+		if(name != null && name.length() > 0) {
+			this.firstName = name;
+			return true;
+		}
+		return false;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	public boolean setLastName(String name) {
+		if(name != null && name.length() > 0) {
+			this.firstName = name;
+			return true;
+		}
+		return false;
+	}
+	
+	public int getPhoneNum() {
+		return this.phoneNum;
+	}
+	public boolean setPhoneNum(int num) {
+		if(String.valueOf(num).length() != 10){
+			return false;
+		}
+		this.phoneNum = num;
+		return true;
+	}
 	
 }
--- src/main/java/com/flexon/app/accountUI.java
+++ src/main/java/com/flexon/app/accountUI.java
@@ -6,66 +6,84 @@ import java.util.Scanner;
 
 public class accountUI {
 	
+	static User user = new User();
+	public static void testEmail() {
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Please input an email:");
+		String userEmail = myObj.nextLine();
+		if(user.setEmail(userEmail)) {
+			System.out.println("Email Done");
+		}else {
+			System.out.println("Email Error");
+		}
+	}
+	
+	public static void testFirstName() {
+		Scanner myObj1 = new Scanner(System.in);
+		System.out.println("Please input your first name:");
+		String firstName = myObj1.nextLine();
+		if(user.setFirstName(firstName)) {
+			System.out.println("FirstName Done");
+		} else{
+			System.out.println("FirstName Error");
+		}
+	}
+	
+	public static void testLastName() {
+		Scanner myObj2 = new Scanner(System.in);
+		System.out.println("Please input your last name:");
+		String lastName = myObj2.nextLine();
+		if(user.setLastName(lastName)) {
+			System.out.println("LastName Done");
+		}else {
+			System.out.println("LastName Error");
+		}
+	}
+	
+	public static void testPassword() {
+		Scanner myObj3 = new Scanner(System.in);
+		System.out.println("Please input your password:");
+		String userPassword = myObj3.nextLine();
+		if(user.setPassword(userPassword)) {
+			System.out.println("Password Done");
+		} else{
+			System.out.println("Password Error");
+		}
+	}
+	
+	public static void testPhoneNum() {
+		Scanner myObj4 = new Scanner(System.in);
+		System.out.println("Please input your phone number:");
+		int phoneNum = myObj4.nextInt();
+		if (user.setPhoneNum(phoneNum)) {
+			System.out.println("PhoneNum Done");
+		} else{
+			System.out.println("PhoneNum Error");
+		} 
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		User user = new User();
 		
 		//Test Email
-		Scanner myObj = new Scanner(System.in);
-		System.out.println("Please input an email:");
-		String userEmail = myObj.nextLine();
-		try {
-			user.setEmail(userEmail);
-			System.out.println("Email Done");
-		} catch(MyException ex){
-			System.out.println(ex.getMessage());
-		}
-			
-		//Test Name
-		Scanner myObj1 = new Scanner(System.in);
-		System.out.println("Please input your username:");
-		String userName = myObj1.nextLine();
-		try {
-			user.setName(userName);
-			System.out.println("Username Done");
-		} catch(MyException ex){
-			System.out.println(ex.getMessage());
-		}
+		testEmail();
 		
-		//Test Gender
-		Scanner myObj2 = new Scanner(System.in);
-		System.out.println("Please input your gender:");
-		String userGender = myObj2.nextLine();
-		try {
-			user.setGender(userGender);
-			System.out.println("Gender Done");
-		}catch(Exception ex) {
-			System.out.println(ex.getMessage());
-		}
+		//Test First Name
+		testFirstName();
+		
+		//Test Last Name
+		testLastName();
 		
 		//Test Password
-		Scanner myObj3 = new Scanner(System.in);
-		System.out.println("Please input your password:");
-		String userPassword = myObj3.nextLine();
-		try {
-			user.setPassword(userPassword);
-			System.out.println("Password Done");
-		} catch(MyException ex){
-			System.out.println(ex.getMessage());
-		}
+		testPassword();
 		
-		//Test Birthday
-		Scanner myObj4 = new Scanner(System.in);
-		System.out.println("Please input your birthday:");
-		String userBirthday = myObj4.nextLine();
-		try {
-			user.setBirthday(userBirthday);
-			System.out.println("Birthday Done");
-		} catch(Exception ex){
-			System.out.println(ex.getMessage());
-		} 
+		//Test PhoneNum
+		testPhoneNum();
 	}
+
+	
 
 	
 
